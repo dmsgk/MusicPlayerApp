85a274dd2f658d837ee2d0074984474a618f3fbf
johyeon yoon <yds03080@naver.com>
2021-06-04 17:51:29 +0900

    Implement play/pause button and progressSlider
    오디오 파일재생/정지, 그에 따른 상태바 조절, 현재재생시간 구현

--- Programmers-MusicPlayerApp/ViewController.swift
+++ Programmers-MusicPlayerApp/ViewController.swift
@@ -23,6 +23,88 @@ class ViewController: UIViewController {
     @IBOutlet var totalTime : UILabel!
     
     
+    // MARK: -IBActions
+    @IBAction func touchUpPlayPauseMusic(_ sender: UIButton) {
+        sender.isSelected = sender.isSelected ? false : true
+        if sender.isSelected {
+            self.player.play()
+            player.addPeriodicTimeObserver(forInterval: CMTimeMake(value: 1, timescale: 10), queue: .main)
+            { time in
+                let rawTime = CMTimeGetSeconds(time)
+                let totalRawTime = CMTimeGetSeconds(self.player.currentItem!.duration)
+                let fraction = rawTime / totalRawTime
+                self.progressSlider.value = Float(fraction)
+                let currTime = self.convertCMTimeToRealTime(seconds: time)
+                self.currentTime?.text = currTime[0] as? String
+            }
+        }
+        else {
+            self.player.pause()
+        }
+    }
+    
+    //MARK: -AudioPlayer
+    func initPlayer(url: URL) {
+        let playerItem = AVPlayerItem(url: url)
+        player = AVPlayer(playerItem: playerItem)
+        playAudioBackground()
+        
+        DispatchQueue.main.async { [self] in
+            self.progressSlider.minimumValue = 0
+            self.progressSlider.value = 0
+            
+            if player.currentItem?.status == .readyToPlay {    //  반영안된다. 나중에 수정하기.
+                let totalSeconds = player.currentItem!.duration
+                print("totalSeconds: \(totalSeconds)")
+                self.totalTime?.text = self.convertCMTimeToRealTime(seconds: totalSeconds)[0] as? String
+            }
+        }
+        
+    
+    }
+    
+    func playAudioBackground() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [.mixWithOthers, .allowAirPlay])
+            print("Playback OK")
+            try AVAudioSession.sharedInstance().setActive(true)
+            print("Session is Active")
+        } catch {
+            print(error)
+        }
+    }
+    func makeAndFireTimer() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [unowned self] (timer: Timer) in
+          
+            if self.progressSlider.isTracking { return }
+//            self.convertCMTimeToRealTime(seconds: self.player.currentTime())
+            self.progressSlider.value = Float(CMTimeGetSeconds(self.player.currentTime()))
+        })
+        self.timer.fire()
+    }
+    
+    func invalidateTimer() {
+        self.timer.invalidate()
+        self.timer = nil
+    }
+    
+    func convertCMTimeToRealTime (seconds : CMTime) -> Array<Any> {
+        let totalSeconds = CMTimeGetSeconds(seconds)
+        let seconds: Int = Int(totalSeconds.truncatingRemainder(dividingBy: 60))
+        let minutes: Int = Int(totalSeconds / 60)
+        let miliseconds: Int = Int(totalSeconds.truncatingRemainder(dividingBy: 1) * 100)
+        let timeFormatString = String(format: "%01d:%02d", minutes, seconds)
+      return [timeFormatString, miliseconds]
+    }
+
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.playPauseButton.isSelected = false
+        self.progressSlider.value = 0
+        self.invalidateTimer()
+    }
+    
+    
+    
     // MARK: -Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,15 +120,15 @@ class ViewController: UIViewController {
                 let jsonDecoder = JSONDecoder()
                 do {
                     let parsedJSON = try jsonDecoder.decode(MusicData.self, from: data)
-//                    let fileUrl = parsedJSON.file
                     let imageUrl = parsedJSON.image
                     guard let imageData = try? Data(contentsOf: imageUrl) else {
                         return
                     }
                     let image = UIImage(data: imageData)
-                   
+                    let fileUrl = parsedJSON.file
+                    self.initPlayer(url: fileUrl)
+                    
                     DispatchQueue.main.async { [self] in
-                        
                         let resizedImage = image?.resizedImage(newWidth:  self.albumCoverImageView.frame.width)
 
                         self.singer?.text = parsedJSON.singer
